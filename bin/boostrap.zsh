812abf8e5ceb67f0ef711a710ba0743ad24d7442
Sean Scurlock <sean.scurlock@gmail.com>
2023-03-07 14:52:28 -0600

    Update boostrap.zsh

--- bin/boostrap.zsh
+++ bin/boostrap.zsh
@@ -4,4 +4,5 @@ xcode-select --install
 python3 -m pip3 install --upgrade pip
 sudo pip3 install ansible
 ansible-galaxy install -r requirements.yml
+
 ansible-playbook -i 'localhost,' -c local ansible_osx.yml --ask-become-pass
