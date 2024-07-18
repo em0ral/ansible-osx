9fd5f5ca357b368c557689e2aa6abc3f0845a870
Sean Scurlock <sean.scurlock@gmail.com>
2024-07-18 09:23:54 -0500

    Update boostrap.zsh
    Edits

--- bin/boostrap.zsh
+++ bin/boostrap.zsh
@@ -1,6 +1,10 @@
 #!bin/zsh
 
 xcode-select --install
+touch /Users/em0ral/.zshrc
+mkdir /Users/em0ral/Repositories
+mkdir /Users/em0ral/.ssh
+mkdir /Users/em0ral/Solana
 python3 -m pip3 install --upgrade pip
 sudo pip3 install ansible
 ansible-galaxy install -r requirements.yml
