43aca853c3f97d7d359457f73310be56cd103f9b
Sean Scurlock <em0ral@seyan.local>
2025-02-21 15:30:02 -0600

    Added signal app

--- bin/boostrap.zsh
+++ bin/boostrap.zsh
@@ -3,7 +3,7 @@
 touch /Users/em0ral/.zshrc
 touch /Users/em0ral/.zprofile
 mkdir /Users/em0ral/.ssh
-mkdir /Users/em0ral/Solana
+
 python3 -m pip3 install --upgrade pip
 sudo pip3 install ansible
 ansible-galaxy install -r requirements.yml
